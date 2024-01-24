c5abdfd4abed61034943315cbec56b97ef30c59c
root <huseynisrafilov17@gmail.com>
2024-01-24 06:11:23 -0800

    style fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4, sys
+    import hidden_4
+    import sys
     secrets = dir(hidden_4)
     for i in secrets:
         if i[0] != "_" or i[1] != "_":
