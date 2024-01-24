259923af9e2e40d2b920cb60000b8bff3b2b1e26
root <huseynisrafilov17@gmail.com>
2024-01-24 05:38:11 -0800

    2nd task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     argv = sys.argv
     if len(argv) - 1 == 0:
         print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
