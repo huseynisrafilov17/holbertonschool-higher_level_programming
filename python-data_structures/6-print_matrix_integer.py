a43e48725c7f249b3230f4f7bca6b7dede3151e4
root <huseynisrafilov17@gmail.com>
2024-01-26 04:10:55 -0800

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,17 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j != i[len(i) - 1]:
-                print("{}".format(j), end=" ")
+                print("{:d}".format(j), end=" ")
             else:
-                print("{}".format(j), end="")
+                print("{:d}".format(j), end="")
         print()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()        
