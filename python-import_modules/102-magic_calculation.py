a06f00af5c8e5edf227db56e045d0677b9cde66b
root <huseynisrafilov17@gmail.com>
2024-01-27 03:45:52 -0800

    fix

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@ def magic_calculation(a, b):
     if a < b:
         from magic_calculation_102 import add, sub
         c = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
         return c
     from magic_calculation_102 import sub
