ca94d000e56335840a89a062cbc61420b1136a5f
root <huseynisrafilov17@gmail.com>
2024-01-30 04:24:17 -0800

    12th task

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -15,18 +15,3 @@ def roman_to_int(roman_string):
             number = number + diction[roman_string[i]]
         i = i + 1
     return number
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
