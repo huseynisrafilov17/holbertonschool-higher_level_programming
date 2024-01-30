a4634053ca4ca7ab2dda17c27646fc849d4439fb
root <huseynisrafilov17@gmail.com>
2024-01-30 04:18:34 -0800

    12th task

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string and isinstance(roman_string, str):
-        return None
+        return 0
     diction = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90,
                "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1}
     length = len(roman_string)
