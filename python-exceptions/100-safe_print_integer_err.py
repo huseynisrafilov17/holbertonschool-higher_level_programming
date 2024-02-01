f045cf80e30533597f9bcf59db0e3f0a94cbb201
root <huseynisrafilov17@gmail.com>
2024-02-01 11:46:01 -0800

    fix

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as error:
-        print("Exception: {}".format(error))
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
     return True
