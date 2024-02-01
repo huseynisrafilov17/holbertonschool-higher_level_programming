eba8a8b785dd65384bc23957599e1121db610e9d
root <huseynisrafilov17@gmail.com>
2024-02-01 04:26:37 -0800

    fox

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    tries = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
+            tries+=1
         except IndexError:
-            i = i - 1
             break
     print()
-    return i + 1
-
-my_list = [1, 2, 3, 4, 5]
+    return tries
