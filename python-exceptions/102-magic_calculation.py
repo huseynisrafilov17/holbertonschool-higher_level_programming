50bab82cbdadb0e86b9ae3c859f56ac012befaad
root <huseynisrafilov17@gmail.com>
2024-02-02 05:38:00 -0800

    9th task

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -5,9 +5,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception("Too far")
             else:
-                result = (a ** b) / i
+                result += (a ** b) / i
         except Exception:
             break
-        else:
-            result = a + b
     return result
