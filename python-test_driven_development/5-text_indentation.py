30cc70c1cd938062902ca5e798cd10812eef74c8
root <huseynisrafilov17@gmail.com>
2024-02-04 06:42:49 -0800

    forgot adding something

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,7 +11,10 @@ def text_indentation(text):
     '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.replace(". ", "\n\n")
-    text = text.replace(": ", "\n\n")
-    text = text.replace("? ", "\n\n")
+    text = text.replace(". ", ".\n\n")
+    text = text.replace(".", ".\n\n")
+    text = text.replace(": ", ":\n\n")
+    text = text.replace(":", ":\n\n")
+    text = text.replace("? ", "?\n\n")
+    text = text.replace("?", "?\n\n")
     print("{}".format(text), end="")
