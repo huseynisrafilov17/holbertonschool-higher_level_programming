ca22af02b32e923c17f2f524df2fd76ffd792cc2
root <huseynisrafilov17@gmail.com>
2024-02-03 21:25:27 -0800

    3rd task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -16,3 +16,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        '''Calculates the area of square'''
+        return self.__size ** 2
