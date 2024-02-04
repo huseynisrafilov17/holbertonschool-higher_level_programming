0f1df4a024b6b6a6a5e158d8db7aa9c2c538caee
root <huseynisrafilov17@gmail.com>
2024-02-04 06:19:26 -0800

    added documentatiion

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+'''
+This module has a function that says your name.
+'''
+
 
 def say_my_name(first_name, last_name=""):
+    '''
+    say_my_name says your name
+
+    Args:
+        first_name: first name
+        last_name: last name
+    '''
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
