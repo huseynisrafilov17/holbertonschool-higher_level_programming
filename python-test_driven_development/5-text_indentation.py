018bfa8775dcb0d61a5952d20684468e3a0b7ac3
root <huseynisrafilov17@gmail.com>
2024-02-04 06:39:52 -0800

    4th task

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-
+'''
+This module includes a function that replace '.', ':', '?' with 2 new lines
+'''
 def text_indentation(text):
+    '''
+    text_indentation replaces '.', ':', '?' with 2 new lines
+
+    Args:
+        text: text
+    '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     text = text.replace(". ", "\n\n")
