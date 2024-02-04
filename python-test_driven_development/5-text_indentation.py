90896d1a295fce523ee73364bd63e2c8b624b733
root <huseynisrafilov17@gmail.com>
2024-02-04 06:38:09 -0800

    4th task

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.replace(". ", "\n\n")
+    text = text.replace(": ", "\n\n")
+    text = text.replace("? ", "\n\n")
+    print("{}".format(text), end="")
