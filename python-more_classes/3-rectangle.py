743865d07ecc507711d204e77d9dce54e1f9c358
root <huseynisrafilov17@gmail.com>
2024-02-04 23:56:53 -0800

    3rd task

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -52,16 +52,3 @@ class Rectangle:
                 if i != self.height - 1:
                     string = string + "\n"
         return string
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
