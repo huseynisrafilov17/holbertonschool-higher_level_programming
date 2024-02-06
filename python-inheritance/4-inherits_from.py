ea111e337a86a136659493d115c2d2edb2975fc5
root <huseynisrafilov17@gmail.com>
2024-02-06 01:28:22 -0800

    4th task

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
         obj: object
         a_class: class
     '''
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
