5dc8a2d505042fab9d2481b66bed517d34bc3155
root <huseynisrafilov17@gmail.com>
2024-02-05 23:27:46 -0800

    0th task

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -5,4 +5,10 @@ This module helps you to lookup into classes
 
 
 def lookup(obj):
+    '''
+    lookup helps you to look into the objects
+
+    Args:
+        obj: object
+    '''
     return dir(obj)
