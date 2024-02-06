84cc5a057c5841fe9e240a453c0fa2aff90cb081
root <huseynisrafilov17@gmail.com>
2024-02-05 23:50:40 -0800

    1st task

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+MyModule
+'''
+class MyList(list):
+    '''
+    List inherited
+    '''
+    def print_sorted(self):
+        new_list = self[:]
+        new_list.sort()
+        print(new_list)
