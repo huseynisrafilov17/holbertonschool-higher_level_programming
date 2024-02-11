e903d25a86688a7fb24096729e8cf50c53149c75
root <huseynisrafilov17@gmail.com>
2024-02-11 01:41:35 -0800

    0th task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+'''
+This module has a function for reading from a file
+'''
+
 def read_file(filename=""):
+    '''
+    read_file reads from a file and prints it
+    Args:
+        filename: file name
+    '''
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
