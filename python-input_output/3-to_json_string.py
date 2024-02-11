72db2085d82f9f784b7303aa5f41162524a4ec3d
root <huseynisrafilov17@gmail.com>
2024-02-11 05:44:58 -0800

    style fix

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,9 +2,9 @@
 '''
 Has a function that returns the JSON representation of an object (string)
 '''
-
-
 import json
+
+
 def to_json_string(my_obj):
     '''
     to_json_string returns the JSON representation of an object
