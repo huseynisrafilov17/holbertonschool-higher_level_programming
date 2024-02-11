d0e0b4584054ad7205b36801b571e86175431317
root <huseynisrafilov17@gmail.com>
2024-02-11 10:47:02 -0800

    fix

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -9,15 +9,18 @@ if __name__ == "__main__":
                     "404": 0, "405": 0, "500": 0}
     file_size = 0
     count = 0
+    same = False
     while True:
         try:
             string = input()
             x = string.split()
             for j in x:
-                if j.isdigit() and j in status_codes.keys():
+                if j.isdigit() and j in status_codes.keys() and same == False:
                     status_codes[j] += 1
+                    same = True
                 elif j.isdigit():
                     file_size += int(j)
+            same = False
             count += 1
             if count == 10:
                 print("File size: {}".format(file_size))
