6517ae885726b8df5ed94d5930229afd75f13469
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-03-24 10:19:17 +0400

    6th task

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates a table.
+CREATE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states 
+(id INT UNIQUE AUTO_INCREMENT = 1 PRIMARY KEY NOT NULL, name VARCHAR(256) NOT NULL);
