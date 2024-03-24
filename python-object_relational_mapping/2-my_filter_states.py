da8bdd3b383aaa10dd6f9966dc317fdb8572d813
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-03-24 17:39:12 +0400

    2nd task

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
                            passwd=args[2], db=args[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute('''SELECT * FROM states 
-                   WHERE name = '{}' ORDER BY id'''.format(args[4]))
+    cur.execute('''SELECT * FROM states
+                   WHERE name LIKE BINARY '{}' ORDER BY id'''.format(args[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
